fbbc751f59e2fda4c89d0cb29bfde6d38e4cc787
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-07-08 10:39:33 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,8 +6,4 @@ bundle exec rails db:chatwoot_prepare
 
 bundle exec rails db:migrate
 
-multirun \
-    "bundle exec sidekiq -C config/sidekiq.yml" \
-    "bundle exec rails s -b 0.0.0.0 -p $PORT"
-
-false
+bundle exec rails s -b 0.0.0.0 -p 3000
