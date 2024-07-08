63d0826b405660511dca0e706f95eb7291a79ddf
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-07-08 10:26:40 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,6 @@
 
 set -e
 
-echo Waiting for database...
-
-while ! pg_isready -h ${PGHOST} -p ${PGPORT}; do sleep 0.25; done; 
-
-echo Database is now available
-
 bundle exec rails db:chatwoot_prepare
 
 bundle exec rails db:migrate
